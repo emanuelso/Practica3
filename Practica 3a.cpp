279781437540002ebc7c124f849ca32d25f13de2
emanuelso <100615147+emanuelso@users.noreply.github.com>
2022-05-25 18:17:13 +0200

    Add files via upload

---
+++ Practica 3a.cpp
@@ -0,0 +1,46 @@
+#include <Arduino.h>
+/*
+ESP32 Web Server - STA Mode
+modified on 25 MAy 2019
+by Mohammadreza Akbari @ Electropeak
+https://electropeak.com/learn
+*/
+#include <WiFi.h>
+#include <WebServer.h>
+extern String HTML;
+
+
+void handle_root();
+// SSID & Password
+const char* ssid = "Xiaomi_11T_Pro"; // Enter your SSID here
+const char* password = "f5cbd8a82232"; //Enter your Password here
+WebServer server(80);
+// Object of WebServer(HTTP port, 80 is defult)
+void setup() {
+Serial.begin(115200);
+Serial.println("Try Connecting to ");
+Serial.println(ssid);
+// Connect to your wi-fi modem
+WiFi.begin(ssid, password);
+// Check wi-fi is connected to wi-fi network
+while (WiFi.status() != WL_CONNECTED) {
+delay(1000);
+Serial.print(".");
+}
+Serial.println("");
+Serial.println("WiFi connected successfully");
+Serial.print("Got IP: ");
+Serial.println(WiFi.localIP()); //Show ESP32 IP on serial
+server.on("/", handle_root);
+server.begin();
+Serial.println("HTTP server started");
+delay(100);
+}
+void loop() {
+server.handleClient();
+}
+// HTML & CSS contents which display on web server
+// Handle root url (/)
+void handle_root() {
+server.send(200, "text/html", HTML);
+}
---
+++ pagina.cpp
@@ -0,0 +1,10 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+
+String HTML = "<!DOCTYPE html>\
+<html>\
+<body>\
+<h1>Esta pagina ha sido modificada;</h1>\
+</body>\
+</html>";
